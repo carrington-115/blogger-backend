c1cd45800b843cee673dbb18fd2abe611453585f
Mark Carrington <frumarkcarrington@gmail.com>
2024-06-14 17:11:33 +0530

    wrote the userModel.js functions to find the user using the username and id

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,34 @@
+const { ObjectId } = require("mongodb");
 const { client } = require("../config/dbConfig");
+const session = client.startSession();
+const accountCollection = client.db("users").collection("accounts");
 
-const findUserByUsername = async () => {};
-const findUserById = async () => {};
+const findUserByUsername = async (username) => {
+  session.startTransaction();
+  try {
+    const user = await accountCollection.findOne({ username: username });
+    await session.commitTransaction();
+    return user;
+  } catch (error) {
+    await session.abortTransaction();
+    console.error(error);
+  } finally {
+    await session.endSession();
+  }
+};
+const findUserById = async (userId) => {
+  session.startTransaction();
+  try {
+    const user = await accountCollection.findOne({ _id: new ObjectId(userId) });
+    await session.commitTransaction();
+    return user;
+  } catch (error) {
+    await session.abortTransaction();
+    console.error(error);
+  } finally {
+    await session.endSession();
+  }
+};
 const createUser = async () => {};
 
 module.exports = { findUserByUsername, findUserById, createUser };
