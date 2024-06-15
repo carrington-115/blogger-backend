fbb70d67eb359806ee711e60a3226f1dfc8f6e03
Mark Carrington <frumarkcarrington@gmail.com>
2024-06-15 09:52:09 +0530

    created a route for handling the email sending with mailjet wrote a middleware function to handle the sending request with the mailjet api

--- controllers/emailServiceController.js
+++ controllers/emailServiceController.js
@@ -1,3 +1,5 @@
+require("dotenv").config({ path: "../.env" });
+const { default: Mailjet } = require("node-mailjet");
 const { sendEmail } = require("../middleware/middleware");
 
 const emailSendingController = (req, res) => {
@@ -12,4 +14,37 @@ const emailSendingController = (req, res) => {
   }
 };
 
-module.exports = { emailSendingController };
+const sendEmailWithMailjet = async (req, res) => {
+  try {
+    const { receiverEmail, receiverName } = req.body;
+    const mailjet = Mailjet.apiConnect(
+      process.env.MAILJET_API_KEY,
+      process.env.MAILJET_SECRET_KEY
+    );
+    const sendResponse = await mailjet
+      .post("send", { version: "v3.1" })
+      .request({
+        Messages: [
+          {
+            From: {
+              Email: "frumarkcarrington@gmail.com",
+              name: "Fru Mark Carrington Chei",
+            },
+            To: [
+              {
+                Email: receiverEmail,
+                Name: receiverName,
+              },
+            ],
+            Subject: "Greetings",
+            TextPart: "I am Mark. Nice to know you. Did the test work",
+          },
+        ],
+      });
+    console.log(sendResponse.body);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = { emailSendingController, sendEmailWithMailjet };
--- routes/emailRouter.js
+++ routes/emailRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const {
   emailSendingController,
+  sendEmailWithMailjet,
 } = require("../controllers/emailServiceController");
 const router = express.Router();
 
 router.post("/send-email", emailSendingController);
+router.post("/send-mailjet", sendEmailWithMailjet);
 
 module.exports = router;
